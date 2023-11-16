9ecbc1ab8e5c14954fd44d01a9e61d97ffaeeb73
AlvirElWapo <correodeandresalvir@gmail.com>
2023-11-15 22:53:41 -0600

    CONSULTAS INICIO; FUNCIONALFALTA AGREGAR CRITERIOS DE BUSQUEDA

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,8 +1,16 @@
 <template>
   <div>
-    <Breadcrumb breadcrumb="" />
-    <div class="mt-8"></div>
+    <div class="mt-4">
+      <label for="idTra">SELECCION DE EQUIPO: </label>
+      <select id="idTra" v-model="selectedIdTra" @change="fetchData">
+        <option v-for="idTra in idTraList" :key="idTra.ID_Tra" :value="idTra.ID_Tra">
+        {{ idTra.ID_Tra }}
+        </option>
+      </select>
+    </div>
 
+
+    <div class="mt-8"></div>
     <div class="flex flex-col mt-8">
       <div class="py-2 -my-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
         <div
@@ -33,7 +41,7 @@
                     bg-gray-50
                   "
                 >
-                  Name
+                 Equipo 
                 </th>
                 <th
                   class="
@@ -49,7 +57,7 @@
                     bg-gray-50
                   "
                 >
-                  Title
+                  Integrantes 
                 </th>
                 <th
                   class="
@@ -65,7 +73,7 @@
                     bg-gray-50
                   "
                 >
-                  Status
+                  Id's Integrantes 
                 </th>
                 <th
                   class="
@@ -81,7 +89,7 @@
                     bg-gray-50
                   "
                 >
-                  Role
+                  Numero De Integrantes
                 </th>
                 <th class="px-6 py-3 border-b border-gray-200 bg-gray-50"></th>
               </tr>
@@ -89,71 +97,37 @@
 
             <tbody class="bg-white">
               <tr v-for="(u, index) in users" :key="index">
-                <td
-                  class="px-6 py-4 border-b border-gray-200 whitespace-nowrap"
-                >
-                  <div class="flex items-center">
-                    <div class="flex-shrink-0 w-10 h-10">
-                      <img
-                        class="w-10 h-10 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
-                    </div>
+<td class="px-6 py-4 border-b border-gray-200 whitespace-nowrap">
+  <div class="text-sm font-medium leading-5 text-gray-900">
+    {{ u.ID_Tra }}
+  </div>
+</td>
 
-                    <div class="ml-4">
-                      <div class="text-sm font-medium leading-5 text-gray-900">
-                        {{ u.name }}
-                      </div>
-                      <div class="text-sm leading-5 text-gray-500">
-                        {{ u.email }}
-                      </div>
-                    </div>
-                  </div>
-                </td>
+<td class="px-6 py-4 border-b border-gray-200 whitespace-nowrap">
+  <div class="text-sm font-medium leading-5 text-gray-900">
+    <div v-for="(ponente, index) in u.Ponentes" :key="index">
+      {{ ponente}}
+      <span v-if="index !== u.Ponentes.length - 1"><br /></span>
+    </div>
+  </div>
+</td>
 
-                <td
-                  class="px-6 py-4 border-b border-gray-200 whitespace-nowrap"
-                >
-                  <div class="text-sm leading-5 text-gray-900">
-                    {{ u.title }}
-                  </div>
-                  <div class="text-sm leading-5 text-gray-500">
-                    {{ u.title2 }}
-                  </div>
-                </td>
+<td class="px-6 py-4 border-b border-gray-200 whitespace-nowrap">
+  <div class="text-sm font-medium leading-5 text-gray-900">
+    <!-- Iterate through each ID in the ID_pons array -->
+    <div v-for="(id, index) in u.ID_pons" :key="index">
+      {{ id }}
+      <!-- Add a line break after each ID, excluding the last one -->
+      <span v-if="index !== u.ID_pons.length - 1"><br /></span>
+    </div>
+  </div>
+</td>
 
-                <td
-                  class="px-6 py-4 border-b border-gray-200 whitespace-nowrap"
-                >
-                  <span
-                    class="
-                      inline-flex
-                      px-2
-                      text-xs
-                      font-semibold
-                      leading-5
-                      text-green-800
-                      bg-green-100
-                      rounded-full
-                    "
-                    >{{ u.status }}</span
-                  >
-                </td>
-
-                <td
-                  class="
-                    px-6
-                    py-4
-                    text-sm
-                    leading-5
-                    text-gray-500
-                    border-b border-gray-200
-                    whitespace-nowrap
-                  "
-                >
-                  {{ u.role }}
-                </td>
+<td class="px-6 py-4 border-b border-gray-200 whitespace-nowrap">
+  <div class="text-sm font-medium leading-5 text-gray-900">
+    {{ u.NoPonentes }}
+  </div>
+</td>
 
                 <td
                   class="
@@ -169,39 +143,7 @@
                 >
                   <div class="flex justify-around">
                     <span class="text-yellow-500 flex justify-center">
-                      <a href="#" class="mx-2 px-2 rounded-md"
-                        ><svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          class="h-5 w-5 text-green-700"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                        >
-                          <path
-                            d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"
-                          />
-                          <path
-                            fill-rule="evenodd"
-                            d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
-                            clip-rule="evenodd"
-                          />
-                        </svg>
-                      </a>
-                      <form method="POST">
-                        <button class="mx-2 px-2 rounded-md">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            class="h-5 w-5 text-red-700"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                          >
-                            <path
-                              fill-rule="evenodd"
-                              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                              clip-rule="evenodd"
-                            />
-                          </svg>
-                        </button>
-                      </form>
+                      <!-- Add your edit and delete buttons here -->
                     </span>
                   </div>
                 </td>
@@ -215,25 +157,48 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-import Breadcrumb from "../partials/Breadcrumb.vue";
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
 interface User {
-  name: string;
-  email: string;
-  title: string;
-  title2: string;
-  status: string;
-  role: string;
+  equipo: string;
+  Ponentes: string[];
+  ID_pons: string[];
+  Num_Integrantes: string;
 }
 
-const testUser: User = {
-  name: "John Doe",
-  email: "john@example.com",
-  title: "Software Engineer",
-  title2: "Web dev",
-  status: "Active",
-  role: "Owner",
-};
+const users = ref<User[]>([]);
+const idTraList = ref<{ ID_Tra: string }[]>([]);
+const selectedIdTra = ref<string | null>(null);
 
-const users = ref<User[]>([...Array(10).keys()].map(() => testUser));
+onMounted(async () => {
+  // Fetch ID_Tra list
+  const idTraResponse = await axios.get<{ ID_Tra: string }[]>('http://localhost:1234/id_tras');
+  idTraList.value = idTraResponse.data;
+
+  // Fetch data for the initially selected ID_Tra
+  if (idTraList.value.length > 0) {
+    selectedIdTra.value = idTraList.value[0].ID_Tra;
+    fetchData();
+  }
+});
+
+const fetchData = async () => {
+  try {
+    if (selectedIdTra.value) {
+      const response = await axios.post<User[]>('http://localhost:1234/informacion_de_equipos', {
+        Id_Trab: selectedIdTra.value,
+      });
+
+      // De-jsonify the Ponentes and ID_pons fields
+      users.value = response.data.map(user => ({
+        ...user,
+        Ponentes: JSON.parse(user.Ponentes),
+        ID_pons: JSON.parse(user.ID_Pons),
+      }));
+    }
+  } catch (error) {
+    console.error('Error fetching user data:', error);
+  }
+};
 </script>
