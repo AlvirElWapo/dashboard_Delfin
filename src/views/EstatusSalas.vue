632f1e02bec5134511cac82f199a084c9353c91f
AlvirElWapo <correodeandresalvir@gmail.com>
2023-12-10 18:01:32 -0600

    IMPLEMENTACION TABLAS, FALTA MODERADORES EMERGENTES

--- src/views/EstatusSalas.vue
+++ src/views/EstatusSalas.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="mainContainer">
+
     <div class="mainContainer_title">
       <label for="ubicacion">Selección de Edificio:</label>
       <select id="ubicacion" v-model="selectedUbicacion" @change="fetchData">
@@ -8,7 +9,13 @@
         </option>
       </select>
     </div>
+<div class="update-button-container">
+    <button @click="refreshData" class="update-button">Actualizar Datos</button>
+  </div>
 
+<!-- TABLA ACTIVOS -->
+  <div class="mainContainer">
+    <label >MODERADORES CONFIRMADOS:</label>
     <div class="mt-8"></div>
     <div class="flex flex-col mt-8">
       <div class="py-2 -my-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
@@ -33,18 +40,16 @@
             </thead>
 
             <tbody class="bg-white">
-              <tr v-for="(user, index) in users" :key="index">
+              <tr v-for="(user, index) in activeUsers" :key="index">
                 <td class="px-6 py-4 border-b border-gray-200 whitespace-nowrap">
                   <div class="text-sm font-medium leading-5 text-gray-900">
                     {{ user.Moderador}}
                   </div>
                 </td>
                 <td class="px-6 py-4 border-b border-gray-200 whitespace-nowrap">
-                  <!-- Placeholder for Sala -->
                   {{user.Sala}}
                 </td>
                 <td class="px-6 py-4 border-b border-gray-200 whitespace-nowrap">
-                  <!-- Placeholder for Area -->
                   {{user.Area_Deseada}} 
                 </td>
                 <td class="px-6 py-4 border-b border-gray-200 whitespace-nowrap">
@@ -67,6 +72,80 @@
       </div>
     </div>
   </div>
+
+
+
+
+
+
+    <div class="mt-8"></div>
+    <div class="flex flex-col mt-8">
+      <div class="py-2 -my-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+        <div class="inline-block min-w-full overflow-hidden align-middle border-b border-gray-200 shadow sm:rounded-lg">
+
+          <label for="ubicacion">MODERADORES INCONFIRMADOS:<br>DAR ATENCIÓN URGENTE </label>
+          <table class="min-w-full">
+            <thead>
+              <tr>
+                <th class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                  Moderador
+                </th>
+                <!-- Remove placeholders for Sala and Area if not needed -->
+                <th class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                  Sala
+                </th>
+                <th class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                  Area
+                </th>
+                <th class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
+                  Estatus
+                </th>
+              </tr>
+            </thead>
+
+            <tbody class="bg-white">
+              <tr v-for="(user, index) in allUsers" :key="index">
+                <td class="px-6 py-4 border-b border-gray-200 whitespace-nowrap">
+                  <div class="text-sm font-medium leading-5 text-gray-900">
+                    {{ user.Moderador}}
+                  </div>
+                </td>
+                <td class="px-6 py-4 border-b border-gray-200 whitespace-nowrap">
+                  {{user.Sala}}
+                </td>
+                <td class="px-6 py-4 border-b border-gray-200 whitespace-nowrap">
+                  {{user.Area_Deseada}} 
+                </td>
+                <td class="px-6 py-4 border-b border-gray-200 whitespace-nowrap">
+                  <!-- Estatus handling -->
+                  <div class="estatus">
+                    <p :class="{ 'text-gray': estado === 'Pendiente','text-green': estado === 'Abierta', 'text-red': estado === 'Cerrada' }">
+                      {{ estado }}
+                    </p>
+                    <select class="desplegable" id="estado" v-model="estado">
+                      <option value="Pendiente">Pendiente</option>
+                      <option value="Abierta">Abierta</option>
+                      <option value="Cerrada">Cerrada</option>
+                    </select>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+
+
 </template>
   
 <script setup lang="ts">
@@ -75,13 +154,22 @@ import axios from 'axios';
 
 interface User {
   Moderador: string;
-  // Add other fields like Sala and Area if they are being fetched
+  Sala: string;
+  Area_Deseada: string;
+  // Add other fields from your backend data structure
+  // Example: Pais, Institucion, etc.
 }
 
-const estado = ref('Pendiente'); // Estado inicial
-const users = ref<User[]>([]);
+const estado = ref('Pendiente');
+const allUsers = ref<User[]>([]);
+const activeUsers = ref<User[]>([]);
 const edificiosList = ref<{ UBICACION: string }[]>([]);
 const selectedUbicacion = ref<string | null>(null);
+
+const refreshData = async () => {
+  await fetchData();
+  await fetchData_Activos();
+};
 
 onMounted(async () => {
   try {
@@ -90,7 +178,8 @@ onMounted(async () => {
 
     if (edificiosList.value.length > 0) {
       selectedUbicacion.value = edificiosList.value[0].UBICACION;
-      fetchData();
+      await fetchData();
+      await fetchData_Activos();
     }
   } catch (error) {
     console.error('Error fetching edificios data:', error);
@@ -103,11 +192,47 @@ const fetchData = async () => {
       const response = await axios.post<User[]>('http://localhost:1234/informacion_por_edificio', {
         UBICACION: selectedUbicacion.value,
       });
-      users.value = response.data;
-      console.log(users.value);
+      allUsers.value = response.data;
     }
   } catch (error) {
-    console.error('Error fetching moderador data:', error);
+    console.error('Error fetching all moderador data:', error);
+  }
+};
+
+const fetchData_Activos = async () => {
+  try {
+    const activeModeradoresResponse = await axios.get<{ ID_Mod: string }[]>('http://localhost:1234/obtener_moderadores_activos');
+    console.log("Moderadores conectados:", activeModeradoresResponse.data);
+
+    // Clear the activeUsers.value once before processing the responses
+    activeUsers.value = [];
+
+    // Process each ID_Mod separately
+    for (const mod of activeModeradoresResponse.data) {
+      try {
+        console.log("Fetching data for ID_Mod:", mod.ID_Mod);
+        const response = await axios.post<User[]>('http://localhost:1234/informacion_por_edificio_eidemod', {
+          UBICACION: selectedUbicacion.value,
+          ID_Mod: mod.ID_Mod,
+        });
+        console.log(`Response for ID_Mod ${mod.ID_Mod}:`, response.data);
+
+        if (Array.isArray(response.data)) {
+          activeUsers.value.push(...response.data);
+        } else {
+          console.error(`Expected an array for response data, got:`, response.data);
+        }
+      } catch (error) {
+        console.error(`Error fetching data for ID_Mod ${mod.ID_Mod}:`, error);
+      }
+    }
+
+    // Filter out active users from allUsers
+    const activeModeradorNames = new Set(activeUsers.value.map(user => user.Moderador));
+    allUsers.value = allUsers.value.filter(user => !activeModeradorNames.has(user.Moderador));
+
+  } catch (error) {
+    console.error('Error fetching active moderadores:', error);
   }
 };
 </script>
